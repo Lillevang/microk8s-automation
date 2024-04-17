863610aae79e28407f27d527c9ec158dd239eb2d
Lillevang <jeppe.lillevang@gmail.com>
2024-04-17 16:23:06 +0200

    Added wait between Kyverno and policy installation

--- scripts/setup_kyverno.sh
+++ scripts/setup_kyverno.sh
@@ -25,6 +25,22 @@ if [ $? -ne 0 ]; then
     echo "Failed to install Kyverno." | tee -a $LOGFILE
     exit 1
 fi
+# Wait for Kyverno to be ready
+echo "Waiting for Kyverno pods to be ready..." | tee -a $LOGFILE
+READY=0
+while [ $READY -eq 0 ]; do
+    # Check if the Kyverno pod is ready
+    KYVERNO_READY=$(kubectl get pods -n kyverno -l app.kubernetes.io/name=kyverno -o jsonpath="{.items[0].status.conditions[?(@.type=='Ready')].status}")
+    if [ "$KYVERNO_READY" == "True" ]; then
+        READY=1
+        echo "Kyverno is ready." | tee -a $LOGFILE
+    else
+        echo "Waiting for Kyverno to become ready..." | tee -a $LOGFILE
+        sleep 10
+    fi
+done
+
+
 
 echo "Installing Kyverno policies..."  | tee -a $LOGFILE
 helm install kyverno-policies kyverno/kyverno-policies -n kyverno
